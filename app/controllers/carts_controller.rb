119d33f355c964aae1e9fb62d569cbeae932372c
cmiller36 <m.calliemiller@gmail.com>
2016-08-28 21:16:15 -0400

    Flatiron Store Project

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,21 @@
 class CartsController < ApplicationController
+  before_action :set_cart, only: [:show, :edit, :update, :destroy, :checkout]
+
+  def show
+    @current_cart = current_user.current_cart
+  end
+
+  def checkout
+    @cart.checkout
+    redirect_to cart_path(@cart)
+  end
+
+  private
+
+
+
+  def set_cart
+    @cart = Cart.find(params[:id])
+  end
 
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,38 @@
 class Cart < ActiveRecord::Base
+  belongs_to :user
+  has_many :line_items
+  has_many :items, through: :line_items
+
+  def total
+    @total ||= line_items.includes(:item).reduce(0) do |sum,l_item|
+      sum + (l_item.item.price * l_item.quantity)
+    end
+  end
+
+  def add_item(item_id)
+    @item = Item.find(item_id)
+    if items.include?(@item)
+      line_item = line_items.find_by(item_id: item_id)
+      line_item.quantity += 1
+    else
+      line_item = line_items.build(item: @item, quantity: 1)
+    end
+    line_item
+  end
+
+  def checkout
+    self.status = "submitted"
+    update_inventory
+  end
+
+   def update_inventory
+    if self.status = "submitted"
+      self.line_items.each do |line_item|
+        line_item.item.inventory -= line_item.quantity
+        line_item.item.save
+        end
+      end
+    end
+     
 
 end
